12c9c959f306cf2c8dfa40d3dfbf2b40a5e6d0c0
Kenny Vega <Kennyveoba@gmail.com>
2021-11-22 21:02:06 -0600

    Funcional

--- Laberinto/Fitness.py
+++ Laberinto/Fitness.py
@@ -1,54 +1,189 @@
 import random
-global pesos 
+global pesos, colores 
 poblacion=[]
 pesos=()
+pesos2=()
 
-def Crear_poblacion(cantidad,imagen):
-    print(len(poblacion))
-    if len(poblacion)==0:
-        Crear_otra_poblacion()
+def Crear_poblacion(cantidad,imagen,tamanho,porcentaje):
+    
+    global colores, pesos2, pesos
+   
+    poblacion=[]
+    pesos=()
+    pesos2=()
+
+    individuos=[]
+    colores= imagen
+
+    #Crea la cantidad de la poblacion que el usuario ingresa
+    numero_individuo=0
     for _ in range(cantidad):
         x= random.randint(0,49)
         y= random.randint(0,49)
         nota= fitness([x,y],imagen)
-        poblacion.append([x,y,nota])
+        individuos.append([x,y,nota,"Primera generacion",numero_individuo])
+        numero_individuo+=1
     
-    seleccion(poblacion,pesos)
-
+    pesos=pesos2
+    pesos2=()
+    poblacion.append(individuos)
+    
+    
+    dato=seleccion(poblacion,pesos,cantidad,porcentaje)
+    poblacion.append(dato)
+    
+    #Crea todas las poblaciones que el usuario ingresa 
+    for l in range(tamanho):
+        pesos=pesos2
+        pesos2=()
+        dato=seleccion([dato],pesos,cantidad,porcentaje)
+        poblacion.append(dato)
+        
+    pesos2=()
+   
     return poblacion
 
 
-def Crear_otra_poblacion():
-    return
-
 
 def fitness(individuo,imagen):
-    global pesos 
-    color= imagen[individuo[0]][individuo[1]]
-    binario = format(individuo[0], "b")
-    print(individuo)
-    print(binario)
-    print(binario[0],"primero")
+    global pesos2 
+    peso = 0
+    color= []
 
+    color.append(imagen[individuo[0]][individuo[1]]) 
+
+
+    if 235 < color[0][0] and 235 < color[0][1] and 235 < color[0][2]:
+        peso += 300 
+
+    for n in range(2):
+        try:
+            #Guarda los datos de los pixeles cerca del punto, en horizontal
+            # y vertical
+            color.append(imagen[individuo[0]-n][individuo[1]])
+            color.append(imagen[individuo[0]+n][individuo[1]])
+            color.append(imagen[individuo[0]][individuo[1]-n])
+            color.append(imagen[individuo[0]][individuo[1]-n])
+
+            #Guarda las coordenadas de los pixeles cerca del punto en
+            #diagonal 
+            color.append(imagen[individuo[0]+n][individuo[1]+n])
+            color.append(imagen[individuo[0]+n][individuo[1]-n])
+            color.append(imagen[individuo[0]-n][individuo[1]+n])
+            color.append(imagen[individuo[0]-n][individuo[1]+n])
+        except:
+            #En caso que no choque con un borde y no tenga datos cerca 
+            color.append(imagen[individuo[0]][individuo[1]])
+       
+
+   
+    #Pone la nota dependiendo del color 
+    for n in color:
+        #negro
+        if n[0]<8 and n[1]<8 and n[2]<8:
+            peso -=50
+
+        #blanco
+        elif 235 < n[0] and 235 < n[1] and 235 < n[2]:
+            peso += 100
+
+        #Colores de la entrada y salida 
+        else:
+            peso += 100
+
+    #En caso que sea un punto negro sin nada cerca 
+    if peso<0:
+        peso=1
+    pesos2+=(peso,)
+    return peso 
+
+#Selecciona 2 padres entre los mejores candidatos y realiza
+#un cruce entre ambos dando como resultado un miembro de la
+#nueva generacion
+def seleccion(poblacion,pesos,cantidad,porcentaje):
+    global colores
+
+    indice=len(poblacion)-1
+    resultado=[]
+    mejores=[]
+    numero_individuo=0
+    #Crea una nueva poblacion con la misma cantidad que la anterior 
+    for hijos in range(cantidad):
+        mejores=[]
+        
+        
+        mejores= (random.choices(poblacion[indice], weights=pesos, k=2))
+        
+        lista_hijos=[]
+        cromosoma=""
+        
+        #Pasa los datos de ambos padres a binario
+        x1=format(mejores[0][0], '08b')
+        x2=format(mejores[1][0], '08b')
+
+        y1=format(mejores[0][1], '08b')
+        y2=format(mejores[1][1], '08b')
+
+        #Crea los valores aleatorios para que mute
+        if random.randint(0,10) >porcentaje:
+            #Cromosoma que muta en X y en Y
+            x= random.randint(0,7) 
+            y= random.randint(0,1)
+        else:
+            #En caso que no haya mutacion
+            x=-1
+            y=-1
+
+        #Combina los valores de ambos padres para x
+        for letra in range(8):
+            if y==0 and x == letra:
+                cromosoma+=str(random.randint(0,1))
+
+            elif letra<4:
+                cromosoma+=x1[letra]
+            else:
+                cromosoma+=x2[letra]
+
+        if binario_a_decimal(cromosoma)>=50:
+            x3=random.randint(0,49)
+        else:
+            x3=binario_a_decimal(cromosoma)
+
+        cromosoma=""
+        
+        #Combina los valores de ambos padres para y
+        for letra in range(8):
+            if y==1 and x == letra:
+                cromosoma+=str(random.randint(0,1))
+
+            if letra<4:
+                cromosoma+=y1[letra]
+            else:
+                cromosoma+=y2[letra]
+        
+
+        if binario_a_decimal(cromosoma)>=50:
+            y4=random.randint(0,49)
+           
+        else:
+            y4=binario_a_decimal(cromosoma)
+            
+
+        notas=fitness([x3,y4],colores)
+        lista_hijos.append(mejores)
+        resultado.append([x3,y4, notas,"#"+str(mejores[0][4])+ ": " +"("+str(mejores[0][0])+ ","+str(mejores[0][1]) +")  #"+str(mejores[1][4])+": ("+str(mejores[1][0])+ ","+str(mejores[1][1])+")",numero_individuo ])
+        lista_hijos=[]
+        numero_individuo+=1
+  
+    return resultado
     
-    if color[0]<5 and color[1]<5 and color[2]<5:
-        pesos+=(0,)
-        return 0
-    elif 240 < color[0] and 240 < color[1] and 240 < color[2]:
-        pesos+=(50,)
-        return 50
-    else:
-        pesos+=(100,)
-        return 100
 
+#Convierte el numero binario a decimal      
+def binario_a_decimal(numero_binario):
+	numero_decimal = 0 
 
-def seleccion(poblacion,pesos):
-    
-     
-    print(random.choices(poblacion, weights=pesos, k=50))
-    return 
+	for posicion, digito_string in enumerate(numero_binario[::-1]):
+		numero_decimal += int(digito_string) * 2 ** posicion
 
-    
-     
-
+	return numero_decimal
 
--- Laberinto/Interfaz.py
+++ Laberinto/Interfaz.py
@@ -5,6 +5,8 @@ from PIL import Image,ImageTk
 from Fitness import Crear_poblacion
 import numpy
 from PIL import Image
+ 
+from tkinter import ttk
 
 #........................Ventana.Principal.................................
 
@@ -24,7 +26,7 @@ imgFondoboton = ImageTk.PhotoImage(imgFondoboton)
 #Obtener la imagen para saber color de los pixeles 
 def get_image(image_path):
     """Get a numpy array of an image so that one can access values[x][y]."""
-    image = Image.open('laberinto.jpg', "r")
+    image = Image.open('laberinto.png', "r")
     width, height = image.size
     pixel_values = list(image.getdata())
     if image.mode == "RGB":
@@ -37,7 +39,7 @@ def get_image(image_path):
     pixel_values = numpy.array(pixel_values).reshape((width, height, channels))
     return pixel_values
 
-imgLab = Image.open('laberinto.jpg')
+imgLab = Image.open('laberinto.png')
 imgLab = imgLab.resize((400, 400), Image.ANTIALIAS) # Redimension (Alto, Ancho)
 imgLab = ImageTk.PhotoImage(imgLab)
 fondo= Label(vPrincipal,image=imgLab).place(x=450,y=100)
@@ -55,15 +57,89 @@ class Markers:
         #fondo= Label(newWindow,image=imgFondoPokebola).place(x=0,y=0)
         #newWindow.iconbitmap('pIcon.ico')
         label= Label(newWindow,fg="black",font=("Arial",13,"bold"),text="Informacion").place(x=75,y=4)
-        label= Label(newWindow,fg="black",font=("Arial",13,"bold"),text="Posicion= x:"+ str(individuo[1]) +"  "+ "y:"+str(individuo[0])+ "nota:"+str(individuo[2])).place(x=20,y=40)
+        label= Label(newWindow,fg="black",font=("Arial",13,"bold"),text="Individuo #"+str(individuo[4])).place(x=20,y=40)
+        label= Label(newWindow,fg="black",font=("Arial",13,"bold"),text="Posicion= x:"+ str(individuo[1]) +"  "+ "y:"+str(individuo[0])).place(x=20,y=80)
+        label= Label(newWindow,fg="black",font=("Arial",13,"bold"),text="Nota="+str(individuo[2])).place(x=20,y=120)
+        label= Label(newWindow,fg="black",font=("Arial",13,"bold"),text="Padres=").place(x=20,y=160)
+        label= Label(newWindow,fg="black",font=("Arial",13,"bold"),text=str(individuo[3])).place(x=20,y=180)
+        
+
+
+# Combobox creation
+n = tk.StringVar()
+monthchoosen = ttk.Combobox(vPrincipal, width = 27, textvariable = n)
+  
+# Adding combobox drop down list
+ 
+  
+monthchoosen.grid(column = 1, row = 5)
+monthchoosen.current()
+monthchoosen.place(relx="0.46",rely="0.07")
+
+def seleccion():
+    fondo= Label(vPrincipal,image=imgLab).place(x=450,y=100)
+    crearMarcadores(datos,monthchoosen.current())
+    
+
+
+btn = ttk.Button(vPrincipal, text="Ver poblacion",command=seleccion)
+btn.place(relx="0.45",rely="0.8")
 
 #Pone los marcadores en el mapa
-def crearMarcadores(marcadores):
-    for punto in marcadores:
+def crearMarcadores(marcadores,indice):
+    
+    for punto in marcadores[indice]:
         marcador = Markers()
-        marcador.Markers(punto[0]*8.05+451, punto[1]*8.05+101,punto)
+        marcador.Markers(punto[1]*8.05+451, punto[0]*8.05+101,punto)
 
-crearMarcadores(Crear_poblacion(50,image))
+
+
+
+m = tk.StringVar()
+porcentaje = ttk.Combobox(vPrincipal, width = 27, textvariable = m)
+# Adding combobox drop down list
+porcentaje['values'] = (' 100%', ' 90%', 
+                          ' 80%',
+                          ' 70%',
+                          ' 60%',
+                          ' 50%',
+                          ' 40%',
+                          ' 30%',
+                          ' 20%',
+                          ' 10%',
+                          ' 0%',)
+  
+porcentaje.grid(column = 1, row = 5)
+porcentaje.current()
+porcentaje.place(relx="0.46",rely="0.6")
+
+
+# Crear caja de texto.
+txtcantidad = ttk.Entry(vPrincipal)
+# Posicionarla en la ventana.
+txtcantidad.place(relx="0.55",rely="0.7")
+
+
+# Crear caja de texto.
+txtpoblacion = ttk.Entry(vPrincipal)
+# Posicionarla en la ventana.
+txtpoblacion.place(relx="0.43",rely="0.7")
+
+
+def crearPoblacion():
+    global datos
+    datos=Crear_poblacion(int(txtpoblacion.get()),image,int(txtcantidad.get()),porcentaje.current())
+
+    lista=[]
+    for n in range (int(txtcantidad.get())):
+        lista.append(str(n))
+    monthchoosen['values']= lista
+
+
+
+btn2 = ttk.Button(vPrincipal, text="Crear poblacion",command=crearPoblacion)
+btn2.place(relx="0.56",rely="0.8")
+
 
 vPrincipal.mainloop()
 
